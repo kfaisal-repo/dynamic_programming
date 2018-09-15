d0f41d739456817410ed6ef1c3c84285d0a334c0
kfaisal-repo <43249840+kfaisal-repo@users.noreply.github.com>
2018-09-15 21:38:06 +0530

    Update oracle_interview_question1.py

--- oracle_interview_question1.py
+++ oracle_interview_question1.py
@@ -7,12 +7,8 @@ for i in string.split(" "):
         dictionary[i]=1
     else:
         dictionary[i] +=1
-desc_list_values=sorted(dictionary.values(),reverse=True)
-
-desc_list_values=desc_list_values[:5]
-
+desc_list_values=sorted(dictionary.values(),reverse=True)[:5]
 final_sort=[]
-
 for i in sorted(set(desc_list_values),reverse=True):
     t= [k for k,v in dictionary.items() if v == i]
     final_sort += sorted(t)
